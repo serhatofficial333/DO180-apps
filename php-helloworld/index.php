2519aff45a6aeef0fe8785ea8da6407b20bcf12c
serhatofficial333 <serhatofficial333@gmail.com>
2022-04-19 08:12:31 +0300

    Changed index page contents3.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
+print "BURASI\n";
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Hello, World! php version is " . PHP_VERSION . "\n";
